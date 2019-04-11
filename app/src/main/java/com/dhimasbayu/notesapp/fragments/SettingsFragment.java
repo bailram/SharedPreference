2ff99fe8a7a584d4a975465895c759cd560de651
bailram <dhimasbayu.9f@gmal.com>
2019-04-11 21:35:13 +0700

    Shared Preference

---
+++ app/src/main/java/com/dhimasbayu/notesapp/fragments/SettingsFragment.java
@@ -0,0 +1,13 @@
+package com.dhimasbayu.notesapp.fragments;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+import com.dhimasbayu.notesapp.R;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
